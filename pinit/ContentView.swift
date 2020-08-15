a3354dda2dae8c8fd77fe63b22815efbe57cb395
Janamajaya Mall <janmajaya.choithram@gmail.com>
2020-08-16 01:10:10 +0800

    Initial Commit

---
+++ pinit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  pinit
+//
+//  Created by Janmajaya Mall on 16/8/2020.
+//  Copyright © 2020 Janmajaya Mall. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ pinitTests/pinitTests.swift
@@ -0,0 +1,34 @@
+//
+//  pinitTests.swift
+//  pinitTests
+//
+//  Created by Janmajaya Mall on 16/8/2020.
+//  Copyright © 2020 Janmajaya Mall. All rights reserved.
+//
+
+import XCTest
+@testable import pinit
+
+class pinitTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
